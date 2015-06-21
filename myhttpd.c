950b8f2b66a55f47d6c85a1592544bb86c48e37b
Jun Uchino <system.jun@gmail.com>
2015-06-21 23:21:44 +0900

    implemented gchist result

--- myhttpd.c
+++ myhttpd.c
@@ -4,6 +4,7 @@
 #include <microhttpd.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "mruby.h"
 #include "mruby/compile.h"
 #include "mruby/string.h"
@@ -13,34 +14,53 @@
 
 static int answer_to_connection (void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls) {
 
+  struct MHD_Response *response;
+  static int aptr;
+  if (0 != strcmp (method, "GET")) return MHD_NO;
+  if (&aptr != *con_cls) {
+    *con_cls = &aptr;
+    return MHD_YES;
+  }
+  *con_cls = NULL;
+
   mrb_state* mrb = mrb_open();
 
-  FILE* f = fopen("/Users/juchino/myruby/src/ujun/mruby_build/mruby-libmicrohttpd/myhttpd.rb", "r");
+  FILE* f = fopen("/Users/juchino/myruby/src/ujun/mruby_build/mruby-myhttpd/myhttpd.rb", "r");
   mrb_load_file(mrb, f);
   fclose(f);
 
   mrb_value res;
 
-  char *s = "";
-  mrb_value str = mrb_str_new(mrb, s, 0);
+  char *s = "centos";
+  mrb_value str = mrb_str_new(mrb, s, strlen(s));
   res = mrb_funcall(mrb, mrb_top_self(mrb), "gchist", 1, str);
 
-  char *page = mrb_str_to_cstr(mrb, res);
-
   // FILE* log = fopen("myhttpd.log", "a");
-  // fprintf(log, "%d\n", RARRAY_LEN(res));
+  // for(int i = 0; i < RARRAY_LEN(res); i++){
+  //   fprintf(log, "%s\n", mrb_str_to_cstr(mrb, mrb_ary_ref(mrb, res, i))); 
+  // }
   // fclose(log);
+
+  char *page = NULL;
+  page = (char *)malloc(sizeof(char));
+  for(int i = 0; i < RARRAY_LEN(res); i++){
+    char *a = mrb_str_to_cstr(mrb, mrb_ary_ref(mrb, res, i)); 
+    page = (char*)realloc(page, strlen(page) + strlen(a) + 1);
+    strcat(page, a);
+  }
 
   mrb_close(mrb);
 
-  struct MHD_Response *response;
 
   int ret;
   response = MHD_create_response_from_buffer (strlen (page), (void *) page, MHD_RESPMEM_PERSISTENT);
   ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
   MHD_destroy_response (response);
+
   return ret; 
 }
+
+
 
 int main (int argc, char *argv[]) {
   struct MHD_Daemon *daemon;
--- myhttpd.rb
+++ myhttpd.rb
@@ -4,17 +4,14 @@ def gchist str
 
   db = SQLite3::Database.new('/Users/juchino/Library/Application Support/Google/Chrome/Default/History')
 
-  if str != ""
-  ¦ result = Array.new()
-  ¦ db.execute("select * from urls where title like '%" + str + "%'" + 'and title not like ""') do |row, column|
-      result.push(row[2])
-    end
-    return result
-  else
-    result = Array.new()
-    db.execute('select * from urls where title not like ""') do |row, column|
-      result.push("* " + row[2])
-    end
-    return result.join("\n")
+  result = Array.new()
+  result.push("<html><body>")
+
+  db.execute("select url, title from urls where title like '%" + str + "%'" + 'and title not like ""') do |row, column|
+    result.push("<a href='" + row[0] + "'>" + row[1] + "</a><br />")
   end
+
+  result.push("</body></html>")
+
+  return result
 end
